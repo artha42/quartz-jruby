7d67ab4a4e908238ef4cc0526b3fe923b473245d
vagmi <vagmi.mudumbai@gmail.com>
2011-01-11 15:07:50 +0530

    the at thing works as well

--- test/test_quartz-jruby.rb
+++ test/test_quartz-jruby.rb
@@ -5,6 +5,6 @@ class TestScheduler
   include Quartz::Scheduler
   schedule(:say_hello_5, :every=>5.seconds) { info "every 5 seconds" }
   schedule(:say_hello_30, :every=>30.seconds) { error "every 30 seconds" }
-
+  schedule(:say_hello_at, :every=>1.day, :at=>'15:06:00') { info "fire at 3:05 pm" }
 end
 TestScheduler.instance.run
